7af3f93053a16613471df5d05840454c48c45d47
joabarzua <joabarzua@yahoo.com>
2020-10-21 21:24:40 -0400

    first commit

---
+++ Scribble Notes/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Scribble_Notes
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelNote = new System.Windows.Forms.Label();
+            this.textNote = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.labelSavedNotesTip = new System.Windows.Forms.Label();
+            this.labelTimeSaved = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelNote
+            // 
+            this.labelNote.AutoSize = true;
+            this.labelNote.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelNote.Location = new System.Drawing.Point(10, 23);
+            this.labelNote.Name = "labelNote";
+            this.labelNote.Size = new System.Drawing.Size(39, 19);
+            this.labelNote.TabIndex = 1;
+            this.labelNote.Text = "Note";
+            // 
+            // textNote
+            // 
+            this.textNote.Location = new System.Drawing.Point(55, 23);
+            this.textNote.Multiline = true;
+            this.textNote.Name = "textNote";
+            this.textNote.Size = new System.Drawing.Size(312, 316);
+            this.textNote.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(395, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(200, 316);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.Text = "dataGridView1";
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.Location = new System.Drawing.Point(55, 386);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(100, 30);
+            this.buttonNew.TabIndex = 5;
+            this.buttonNew.Text = "New";
+            this.buttonNew.UseVisualStyleBackColor = true;
+            this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(181, 386);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(100, 30);
+            this.buttonSave.TabIndex = 6;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(495, 386);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(100, 30);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // labelSavedNotesTip
+            // 
+            this.labelSavedNotesTip.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelSavedNotesTip.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.labelSavedNotesTip.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelSavedNotesTip.ForeColor = System.Drawing.Color.White;
+            this.labelSavedNotesTip.Location = new System.Drawing.Point(431, 145);
+            this.labelSavedNotesTip.Name = "labelSavedNotesTip";
+            this.labelSavedNotesTip.Size = new System.Drawing.Size(132, 65);
+            this.labelSavedNotesTip.TabIndex = 11;
+            this.labelSavedNotesTip.Text = "Saved notes appear here";
+            this.labelSavedNotesTip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTimeSaved
+            // 
+            this.labelTimeSaved.AutoSize = true;
+            this.labelTimeSaved.Location = new System.Drawing.Point(55, 342);
+            this.labelTimeSaved.Name = "labelTimeSaved";
+            this.labelTimeSaved.Size = new System.Drawing.Size(0, 15);
+            this.labelTimeSaved.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.labelTimeSaved);
+            this.Controls.Add(this.labelSavedNotesTip);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonNew);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textNote);
+            this.Controls.Add(this.labelNote);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(640, 480);
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "Form1";
+            this.Text = "Scribble Notes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelNote;
+        private System.Windows.Forms.TextBox textNote;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Label labelSavedNotesTip;
+        private System.Windows.Forms.Label labelTimeSaved;
+    }
+}
+
---
+++ Scribble Notes/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scribble_Notes
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Notes", typeof(String));
+            table.Columns.Add("Time", typeof(DateTime));
+
+            dataGridView1.DataSource = table;
+
+            dataGridView1.Columns["Notes"].Width = 125;
+            dataGridView1.Columns["Time"].Width = 72;
+        }
+
+        private void SaveNote()
+        {
+            //Check for null or whitespace to prevent saving empty notes.
+            if (string.IsNullOrWhiteSpace(textNote.Text))
+            {
+                return;
+            }
+
+            DataRow newRow = table.NewRow();
+
+            newRow["Notes"] = textNote.Text;
+            newRow["Time"] = DateTime.Now;
+
+            table.Rows.Add(newRow);
+
+            textNote.Clear();
+            labelTimeSaved.Text = "";
+            labelSavedNotesTip.Hide();
+        }
+
+        private void ClearNoteText()
+        {
+            textNote.Clear();
+            labelTimeSaved.Text = "";
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            //Check for null or whitespace...
+            if (string.IsNullOrWhiteSpace(textNote.Text))
+            {
+                ClearNoteText();
+            }
+            else
+            {
+                //...if text is not null, ask user if they want to save their text before starting a new one.
+                string msgBoxContent = "You are creating a new note without saving your current one. Would you like to save this note?";
+
+                string msgBoxTitle = "Warning";
+
+                var msgBoxResult = MessageBox.Show(msgBoxContent, msgBoxTitle, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+
+                if (msgBoxResult == DialogResult.Yes)
+                {
+                    SaveNote();
+                }
+                else if (msgBoxResult == DialogResult.No)
+                {
+                    ClearNoteText();
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveNote();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            //Check with user before deleting note.
+            string msgBoxContent = "Are you sure you want to delete this note?";
+
+            string msgBoxTitle = "Warning";
+
+            var msgBoxResult = MessageBox.Show(msgBoxContent, msgBoxTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (msgBoxResult == DialogResult.Yes)
+            {
+                int index = dataGridView1.CurrentCell.RowIndex;
+
+                table.Rows[index].Delete();
+
+                ClearNoteText();
+            }
+            else if (msgBoxResult == DialogResult.No)
+            {
+                return;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            DateTime date = table.Rows[index].Field<DateTime>(1);
+
+            Trace.Write(date);
+
+            if (index > -1)
+            {
+                textNote.Text = table.Rows[index].ItemArray[0].ToString();
+                labelTimeSaved.Text = "This note was saved " + date.DayOfWeek + " " + date.ToShortDateString() + " at " + date.ToShortTimeString() + ".";
+            }
+        }
+    }
+}
